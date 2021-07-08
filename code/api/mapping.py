c7f428b5515325d73f3c87c2051c2e460773454d
Taras Maliuchenko <tmali@softserveinc.com>
2021-07-08 18:03:20 +0300

    Fix user agent Change source and source uri for sightings of observables

--- code/api/mapping.py
+++ code/api/mapping.py
@@ -106,7 +106,7 @@ class Mapping:
             JUDGEMENT:
                 lambda: source,
             SIGHTING_OF_OBSERVABLE:
-                lambda: entity['source']
+                lambda: 'Recorded Future Recent Reference'
         }
         return sources[self.name]()
 
@@ -119,7 +119,7 @@ class Mapping:
             JUDGEMENT:
                 lambda: self.lookup['data'].get('intelCard'),
             SIGHTING_OF_OBSERVABLE:
-                lambda: entity.get('url')
+                lambda: self.lookup['data'].get('intelCard')
         }
 
         return uri[self.name]()
